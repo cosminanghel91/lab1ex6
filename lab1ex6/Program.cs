2b6e71bb271b6bfc08dcea5b3a2338768b1ebbfc
Cosmin Anghel <cosmin.anghel@metaplast.ro>
2024-02-19 15:48:07 +0200

    Add project files.

---
+++ lab1ex6/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+* Scrieti un program care va verifica daca un numar citit de la tastatura este par sau impar.
+* Daca numarul este par, programul va afisa "par", iar in caz contrar "impar"
+*/
+
+Console.WriteLine("Introduceti numarul pe care doriti sa il verificam daca este par sau impar");
+int numar = int.Parse(Console.ReadLine());
+
+
+if (numar % 2 == 0)
+{
+    Console.WriteLine("par");
+}
+else
+{
+    Console.WriteLine("impar");
+}
